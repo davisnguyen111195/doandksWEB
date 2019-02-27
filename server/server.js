298aadad0267e5bbbc1f4ec823c7d185af8e219d
davisnguyen111195 <nguyendat111195@gmail.com>
2019-02-28 05:01:51 +0700

    add project

--- server/server.js
+++ server/server.js
@@ -1,12 +1,35 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
 const port = process.env.port || 3000;
 const publicPath = path.join(__dirname, '/../public');
-
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
+  
+io.on('connection', (socket) => {
+    console.log('A new user just connected');
 
-app.listen(port, () => {
+    socket.on('onClick', () => {
+        socket.broadcast.emit('MOTOR', 500)
+        console.log('500');    
+    })
+    
+    
+    socket.on('disconnect', () => {
+        console.log('disconnect');
+    })
+
+    
+}); 
+
+
+
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 })
